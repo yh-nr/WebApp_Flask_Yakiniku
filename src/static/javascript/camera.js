f72dd2adf8e720b559a516b6d2c11da3c61a2f31
yh-nr <ynaru911@gmail.com>
2023-08-18 18:08:30 +0900

    add camera.js

--- src/static/javascript/camera.js
+++ src/static/javascript/camera.js
@@ -1,10 +1,19 @@
 (async function() {
     const videoElement = document.getElementById('video');
+    const canvasElement = document.getElementById('canvas');
+    const snapshotButton = document.getElementById('snapshot');
+        
+    const context = canvasElement.getContext('2d');
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    videoElement.srcObject = stream;
     
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        videoElement.srcObject = stream;
-    } catch (err) {
-        console.error('Error accessing the camera:', err);
-    }
+    snapshotButton.addEventListener('click', () => {
+        context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+        const dataURL = canvasElement.toDataURL('image/png');
+        const downloadLink = document.createElement('a');
+        downloadLink.href = dataURL;
+        downloadLink.download = 'snapshot.png';
+        downloadLink.click();
+    });
+    
 })();
